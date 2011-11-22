816d0e17d2edca1a0ec7f41c73e8409511fddc15
Matt Merrill <mattpmerrill@gmail.com>
2011-11-21 22:55:13 -0700

    Upload stuff
    Signed-off-by: Matt Merrill <mattpmerrill@gmail.com>

---
+++ Avocado.Web/Models/ProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avocado.Web.Models
+{
+    public class ProfileViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Location { get; set; }
+        public string PersonalUrl { get; set; }
+        public string Bio { get; set; }
+    }
+}
